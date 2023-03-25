a8f0695c10ce5c9328ac6b0366009e1d6b816f6c
R. Kukuh <rkukuh@gmail.com>
2023-03-26 00:39:43 +0700

    Fix #1

--- Tests/SwiftCollectionsTests/Chunk/SetChunkedTests.swift
+++ Tests/SwiftCollectionsTests/Chunk/SetChunkedTests.swift
@@ -17,29 +17,29 @@ class SetChunkedTests: XCTestCase {
     
     func testChunkedSet() {
         let set: Set<Int> = [1, 2, 3, 4, 5, 6, 7]
-        let chunkedSet = set.chunked(by: 4)
+        let chunkedSet = set.sorted().chunked(by: 4)
         let expectedChunks = [
-            Set([1, 2, 3, 4]),
-            Set([5, 6, 7])
+            [1, 2, 3, 4],
+            [5, 6, 7]
         ]
-        XCTAssertEqual(chunkedSet.map(Set.init), expectedChunks, "The chunked array should match the expected chunks.")
+        XCTAssertEqual(chunkedSet, expectedChunks, "The chunked array should match the expected chunks.")
     }
     
     func testChunkedSetWithSizeLargerThanSet() {
         let set: Set<Int> = [1, 2, 3, 4, 5, 6, 7]
-        let chunkedSet = set.chunked(by: 10)
-        let expectedChunks: [Set<Int>] = [
-            Set([1, 2, 3, 4, 5, 6, 7])
+        let chunkedSet = set.sorted().chunked(by: 10)
+        let expectedChunks: [[Int]] = [
+            [1, 2, 3, 4, 5, 6, 7]
         ]
-        XCTAssertEqual(chunkedSet.map(Set.init), expectedChunks, "The chunked array should match the expected chunks.")
+        XCTAssertEqual(chunkedSet, expectedChunks, "The chunked array should match the expected chunks.")
     }
     
     func testChunkedSetWithSizeOne() {
         let set: Set<Int> = [1, 2, 3, 4, 5, 6, 7]
-        let chunkedSet = set.chunked(by: 1)
-        let expectedChunks: [Set<Int>] = [
-            Set([1]), Set([2]), Set([3]), Set([4]), Set([5]), Set([6]), Set([7])
+        let chunkedSet = set.sorted().chunked(by: 1)
+        let expectedChunks: [[Int]] = [
+            [1], [2], [3], [4], [5], [6], [7]
         ]
-        XCTAssertEqual(chunkedSet.map(Set.init), expectedChunks, "The chunked array should match the expected chunks.")
+        XCTAssertEqual(chunkedSet, expectedChunks, "The chunked array should match the expected chunks.")
     }
 }
