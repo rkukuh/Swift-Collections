d0c312d82a7cba8d50183b89ccd0c54d29eb797a
R. Kukuh <rkukuh@gmail.com>
2023-03-26 19:38:07 +0700

    Fix #12

--- Sources/SwiftCollections/Average/DictionaryAverageExtension.swift
+++ Sources/SwiftCollections/Average/DictionaryAverageExtension.swift
@@ -21,33 +21,62 @@ public extension Dictionary where Value: Numeric {
     }
 }
 
-public extension Dictionary where Value == [Double] {
-    func average(forKey key: Key) -> Double? {
+public extension Dictionary where Value: Collection, Value.Element: BinaryFloatingPoint {
+    func average(forKey key: Key) -> Value.Element? {
         guard let values = self[key], !values.isEmpty else {
             return nil
         }
-        
+
         let sum = values.reduce(0, +)
-        
-        return sum / Double(values.count)
+        return sum / Value.Element(values.count)
     }
 }
 
-public extension Dictionary where Key == String, Value == [Double] {
-    func averages() -> [String: Double] {
-        var result: [String: Double] = [:]
-        
+public extension Dictionary where Key == String, Value: Collection, Value.Element: BinaryFloatingPoint {
+    func averages() -> [String: Value.Element] {
+        var result: [String: Value.Element] = [:]
+
         for (key, values) in self {
             guard !values.isEmpty else {
                 continue
             }
-            
+
             let sum = values.reduce(0, +)
-            let average = sum / Double(values.count)
-            
+            let average = sum / Value.Element(values.count)
+
             result[key] = average
         }
-        
+
+        return result
+    }
+}
+
+public extension Dictionary where Value: Collection, Value.Element: BinaryInteger {
+    func average(forKey key: Key) -> Double? {
+        guard let values = self[key], !values.isEmpty else {
+            return nil
+        }
+
+        let sum = values.reduce(0, +)
+        return Double(sum) / Double(values.count)
+    }
+}
+
+public extension Dictionary where Key == String, Value: Collection, Value.Element: BinaryInteger {
+    func averages() -> [String: Double] {
+        var result: [String: Double] = [:]
+
+        for (key, values) in self {
+            guard !values.isEmpty else {
+                continue
+            }
+
+            let sum = values.reduce(0, +)
+            let average = Double(sum) / Double(values.count)
+
+            result[key] = average
+        }
+
         return result
     }
 }
